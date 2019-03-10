83838155ee8f4fb03ab114b8976ef1543e9db881
Andrew Johnson <drewbey@gmail.com>
2019-03-09 17:25:25 -0700

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Provider store={store}>    
+        <App />
+    </Provider>    
+    , document.getElementById('root'));
+            registerServiceWorker();
 
---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { createStore } from 'redux';
+import reducer from './ducks/reducer';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default createStore(reducer); 
